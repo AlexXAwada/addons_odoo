209940de671a37386c99a9684358b89bdcc5435a
Alexandre Awada <alexandreawada@hotmail.com>
2025-06-18 16:01:07 -0300

    test module

---
+++ customs_addons/__manifest__.py
@@ -0,0 +1,12 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Test Module for Odoo",
+    "author": "FFA",
+    "version": "15.0.0.1",
+    "category": "Test/Tools",
+    "description": """My first test module for Odoo""",
+    "depends": [],
+    "installable": True,
+    "auto_install": False,
+}
