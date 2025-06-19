ff524d96d7870f4ce437b4c9952fbc2ca2825d4e
Alexandre Awada <alexandreawada@hotmail.com>
2025-06-19 16:40:09 -0300

    [ADD] Registro logistica

--- customs_addons/__manifest__.py
+++ customs_addons/__manifest__.py
@@ -1,12 +1,23 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
-    "name": "Test Module for Odoo",
+    "name": "Regulatorio de Logística",
     "author": "FFA",
     "version": "15.0.0.1",
-    "category": "Test/Tools",
-    "description": """My first test module for Odoo""",
-    "depends": [],
+    "category": "Inventory/Logistics",
+    "summary": "Módulo para agregar la funcionalidad de regulatorio de logística",
+    "description": """
+Este módulo agrega funcionalidad de regulatorio de logística a los productos,
+incluyendo campos de fecha de inicio para control de inventario.
+
+Características:
+- Campo de fecha de inicio en productos
+- Vistas mejoradas para gestión logística
+    """,
+    "depends": ["product"],
+    "data": [
+        "views/registro_logistica.xml",
+    ],
     "installable": True,
     "auto_install": False,
+    "application": False,
+    "license": "LGPL-3",
 }
